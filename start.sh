d96b0651d26f123c43855b1beab23afbfb1589d8
Jelle Z. <public@jellezwie.rs>
2025-07-26 17:11:27 +0200

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 set -e
 
-# Run SteamCMD commands
+# Run SteamCMD to install/update Tower Unite
 /bin/steamcmd +force_install_dir /data +login anonymous +app_update 439660 validate +quit
 
-# Copy required shared object
+# Copy the steamclient.so file (if it exists)
 cp /data/linux64/steamclient.so /data/Tower/Binaries/Linux/ || echo "steamclient.so not found, skipping copy"
 
-# Change working directory
+# Change to game directory
 cd /data
 
-# Start the server with the given map
-./Tower/Binaries/Linux/TowerServer-Linux-Shipping "$MAP_NAME" -log -Port=7777 -QueryPort=27015 -nosteamclient
+# Run the game server
+exec ./Tower/Binaries/Linux/TowerServer-Linux-Shipping "$MAP_NAME" -log -Port=7777 -QueryPort=27015 -nosteamclient
