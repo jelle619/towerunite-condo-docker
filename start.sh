4318e14cb72407b5e3fc19ec15a7787464307d69
Jelle Z. <public@jellezwie.rs>
2025-07-26 17:21:06 +0200

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,14 +1,16 @@
 #!/bin/bash
 set -e
 
-# Run SteamCMD to install/update Tower Unite
+# Run SteamCMD as root (writes to /root/.local/share/Steam)
+echo ">> Running SteamCMD as root..."
 /bin/steamcmd +force_install_dir /data +login anonymous +app_update 439660 validate +quit
 
-# Copy the steamclient.so file (if it exists)
+# Set ownership of game files to 'tower' so it can launch the game
+chown -R tower:tower /data
+
+# Copy steamclient.so if available
 cp /data/linux64/steamclient.so /data/Tower/Binaries/Linux/ || echo "steamclient.so not found, skipping copy"
 
-# Change to game directory
-cd /data
-
-# Run the game server
-exec ./Tower/Binaries/Linux/TowerServer-Linux-Shipping "$MAP_NAME" -log -Port=7777 -QueryPort=27015 -nosteamclient
+# Drop privileges and run the game as 'tower'
+echo ">> Launching Tower Unite server as user 'tower'..."
+exec su-exec tower /data/Tower/Binaries/Linux/TowerServer-Linux-Shipping "$MAP_NAME" -log -Port=7777 -QueryPort=27015 -nosteamclient
